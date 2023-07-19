6d6119ada51b16ec9939a8b063bd02a366ae0fa3
shingomorita1234 <antenakakko@gmail.com>
2023-07-19 15:18:56 +0900

    球が四方向に
    球が四方向に飛ぶようにしました。

--- Assets/Private_Morita/scripts/MoveController.cs
+++ Assets/Private_Morita/scripts/MoveController.cs
@@ -10,6 +10,12 @@ public class MoveController : MonoBehaviour
     private Rigidbody2D rigidBody;
     private Vector2 inputAxis;
 
+    //direction = 0 -> 上
+    //direction = 1 -> 右
+    //direction = 2 -> 下
+    //direction = 3 -> 左
+    public int direction = 0;
+
     void Start()
     {
         // オブジェクトに設定しているRigidbody2Dの参照を取得する
@@ -20,13 +26,68 @@ public class MoveController : MonoBehaviour
     {
         // x,ｙの入力値を得る
         // それぞれ+や-の値と入力の関連付けはInput Managerで設定されている
-        inputAxis.x = Input.GetAxis("Horizontal");
-        inputAxis.y = Input.GetAxis("Vertical");
-        inputAxis.x = Input.
-        inputAxis.y = Input.Get
+        //inputAxis.x = Input.GetAxis("Horizontal");
+        //inputAxis.y = Input.GetAxis("Vertical");
+
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            inputAxis.y = 1;
+            direction = 0;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            inputAxis.y = -1;
+            direction = 2;
+        }
+        else
+        {
+            inputAxis.y = 0;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            inputAxis.x = 1;
+            direction = 1;
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            inputAxis.x = -1;
+            direction = 3;
+        }
+        else
+        {
+            inputAxis.x = 0;
+        }
+      //  Debug.Log("inoutAxis => " + this.inputAxis);
+       // inputAxis.x = Input.
+       // inputAxis.y = Input.Get
+
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Instantiate(PunchPrefab, transform.position, Quaternion.identity);
+           GameObject g = Instantiate(PunchPrefab, transform.position, Quaternion.identity);
+
+            Vector2 vec2 = Vector2.zero;
+
+            //ここに、方向（direction）によって、vec2に値を代入する処理を書く。
+
+            if (direction == 0)
+            {
+                vec2 = new Vector2(0, 1);
+            }
+            else if (direction == 1)
+            {
+                vec2 = new Vector2(1, 0);
+            }
+            else if (direction == 2)
+            {
+                vec2 = new Vector2(0, -1);
+            }
+            else if (direction == 3)
+            {
+                vec2 = new Vector2(-1, 0);
+            }
+            g.GetComponent<Rigidbody2D>().velocity = vec2 * 3;
         }
     }
 
