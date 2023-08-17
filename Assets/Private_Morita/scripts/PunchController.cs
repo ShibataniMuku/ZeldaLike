2a8331bf5e8b7e5d41cd8b67cba8c6f7aaa92270
shingomorita1234 <antenakakko@gmail.com>
2023-08-17 19:30:15 +0900

    球の速度を変えました
    球の速度を変えました

--- Assets/Private_Morita/scripts/PunchController.cs
+++ Assets/Private_Morita/scripts/PunchController.cs
@@ -5,7 +5,7 @@ public class PunchController : MonoBehaviour
 {
 	void Update()
 	{
-		//transform.Translate(0, 0.2f, 0);
+		//transform.Translate(0, 0.10f, 0);
         //if (transform.position.y > 5)
 
 		
