9023c1f1e6b16c24d4190edfe8d8c5f78af03ebc
shingomorita1234 <antenakakko@gmail.com>
2023-10-05 19:57:54 +0900

    ダメージ関数を付けれたと思います
    ダメージ関数を付けれたと思います

---
+++ Assets/Private_Morita/scripts/PlayerStatus.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStatus : MonoBehaviour
+{
+
+   [SerializeField] private int HP = 100;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(string.Format("HP => {0}", HP));
+        decreaseHP(10);
+
+        Debug.Log(string.Format("HP => {0}", HP));
+        decreaseHP(-10);
+        Debug.Log(string.Format("HP => {0}", HP));
+    }
+    public void decreaseHP(int amount)
+    {
+        Debug.Log("decreaseHP‚ª‚æ‚Î‚ê‚½");
+        //HP = HP - amount;
+        HP -= Mathf.Abs(amount);
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
