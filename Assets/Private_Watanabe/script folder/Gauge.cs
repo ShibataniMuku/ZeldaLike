0bd3d978b62309fc2c1ca827d7f7008286282265
hideaki-w <138851060+hideaki-w@users.noreply.github.com>
2023-10-12 20:33:35 +0900

    HP変数の変更
    hpの変数をcurrentからHPに変更しました。

--- Assets/Private_Watanabe/script folder/Gauge.cs
+++ Assets/Private_Watanabe/script folder/Gauge.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class Gauge : MonoBehaviour
 {
     public int max;
-    public int current;
+    public int HP;
     public Image curImg;
     // Start is called before the first frame update
     void Start()
@@ -19,17 +19,17 @@ public class Gauge : MonoBehaviour
     {
         if(max > 0)
         {
-            if(current > max)
+            if(HP > max)
             {
                 curImg.GetComponent<RectTransform>().localScale = new Vector3(1f, 1f, 1f);
             }
-            else if(current < 0)
+            else if(HP < 0)
             {
                 curImg.GetComponent<RectTransform>().localScale = new Vector3(0f, 1f, 1f);
             }
             else 
             {
-                float hpRate = (float)current / (float)max;
+                float hpRate = (float)HP / (float)max;
                 curImg.GetComponent<RectTransform>().localScale = new Vector3(hpRate, 1f,1f);
             }
         }
