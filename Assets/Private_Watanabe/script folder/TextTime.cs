ab0b8c5dbf38089c7c2808265fc853dae79e23b6
hideaki-w <138851060+hideaki-w@users.noreply.github.com>
2023-11-02 21:59:29 +0900

    ボタン追加
    リザルト画面のステージセレクト画面へ移るボタンを追加しました。

--- Assets/Private_Watanabe/script folder/TextTime.cs
+++ Assets/Private_Watanabe/script folder/TextTime.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SocialPlatforms.Impl;
+
 
 public class TextTime : MonoBehaviour
 {
+    private TextMeshProUGUI clearText;
+    private int clearTime = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        clearText = GetComponent<TextMeshProUGUI>();
+        PlayerPrefs.GetInt("ClearTime",clearTime );
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        clearText.text = "Clear Time:" + clearTime.ToString();
     }
 }
